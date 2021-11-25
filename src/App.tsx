2f6dccbb6e0712254cb773b4249b731ab4e4f91c
solebellsBEACH <lucassxxavier@gmail.com>
2021-11-24 22:02:46 -0300

    feat:create Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { RoutesComponent } from "./routes";
 
 function App() {
-  return <div className="App">App</div>;
+  return <RoutesComponent />;
 }
 
 export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+export const RoutesComponent: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" component={Authentication} exact /> */}
+      </Routes>
+    </Router>
+  );
+};
