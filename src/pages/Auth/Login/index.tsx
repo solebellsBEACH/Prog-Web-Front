608d655dbbd2c3efb3baf08fa60056a865fb0406
solebellsBEACH <lucassxxavier@gmail.com>
2021-11-24 22:11:43 -0300

    feat:create LogInRoute

---
+++ src/pages/Auth/Login/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "./styles";
+
+export const Login = () => {
+  return <Container>LogInPage</Container>;
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Login } from "./pages/Auth/Login";
 
 export const RoutesComponent: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" component={Authentication} exact /> */}
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
