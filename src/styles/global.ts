b6f06e1a7dd04db62c4313325d89fbcfe033623e
solebellsBEACH <lucassxxavier@gmail.com>
2021-11-24 22:13:10 -0300

    feat:create globalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+    }
+    button{
+        cursor: pointer;
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+    }
+`;
